fbc5e93ed2ce3c2670451a4f7e1363bdcebc34af
Arita Yuta <k104009y@mail.kyutech.jp>
2016-02-14 07:44:50 +0900

    delete color setting for company-mode
    because monokai support company-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -299,21 +299,6 @@
 (define-key company-active-map [tab] 'company-complete-common2)
 ;; (define-key company-active-map [backtab] 'company-select-previous) 
 
-(set-face-attribute 'company-tooltip nil
-                    :foreground "black" :background "lightgrey")
-(set-face-attribute 'company-tooltip-common nil
-                    :foreground "black" :background "lightgrey")
-(set-face-attribute 'company-tooltip-common-selection nil
-                    :foreground "white" :background "steelblue")
-(set-face-attribute 'company-tooltip-selection nil
-                    :foreground "black" :background "steelblue")
-(set-face-attribute 'company-preview-common nil
-                    :background nil :foreground "lightgrey" :underline t)
-(set-face-attribute 'company-scrollbar-fg nil
-                    :background "DarkCyan")
-(set-face-attribute 'company-scrollbar-bg nil
-                    :background "gray40")
-
 ;; for c++ 
 (require 'irony)
 (eval-after-load "irony"
