ea4f9ae52e3a06e09b5ab84cccbf53a4a3cf28dc
Arita Yuta <k104009y@mail.kyutech.jp>
2016-02-27 20:24:09 +0900

    python-modeのときにcompany-modeが起動してなかったのを修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -277,6 +277,7 @@
 (require 'company)
 (add-hook 'c++-mode-hook 'company-mode)
 (add-hook 'c-mode-hook 'company-mode)
+(add-hook 'python-mode-hook 'company-mode)
 (setq company-idle-delay 0) ; デフォルトは0.5
 (setq company-minimum-prefix-length 1) ; デフォルトは4
 (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
