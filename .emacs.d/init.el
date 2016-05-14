ce2734e0e7bdd3bf8f801025a922341ae7f148e9
Arita Yuta <k104009y@mail.kyutech.jp>
2016-05-14 22:46:09 +0900

    companyの設定を見直し

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -291,31 +291,35 @@
 (setq company-idle-delay 0) ; デフォルトは0.5
 (setq company-minimum-prefix-length 1) ; デフォルトは4
 (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
-
+(setq company-transformers '(company-sort-by-statistics company-sort-by-backend-importance))
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
 (define-key company-search-map (kbd "C-n") 'company-select-next)
 (define-key company-search-map (kbd "C-p") 'company-select-previous)
 
-(defun company--insert-candidate2 (candidate)
-  (when (> (length candidate) 0)
-    (setq candidate (substring-no-properties candidate))
-    (if (eq (company-call-backend 'ignore-case) 'keep-prefix)
-        (insert (company-strip-prefix candidate))
-      (if (equal company-prefix candidate)
-          (company-select-next)
-          (delete-region (- (point) (length company-prefix)) (point))
-        (insert candidate))
-      )))
-(defun company-complete-common2 ()
-  (interactive)
-  (when (company-manual-begin)
-    (if (and (not (cdr company-candidates))
-             (equal company-common (car company-candidates)))
-        (company-complete-selection)
-      (company--insert-candidate2 company-common))))
+;; (defun company--insert-candidate2 (candidate)
+;;   (when (> (length candidate) 0)
+;;     (setq candidate (substring-no-properties candidate))
+;;     (if (eq (company-call-backend 'ignore-case) 'keep-prefix)
+;;         (insert (company-strip-prefix candidate))
+;;       (if (equal company-prefix candidate)
+;;           (company-select-next)
+;;           (delete-region (- (point) (length company-prefix)) (point))
+;;         (insert candidate))
+;;       )))
+;; (defun company-complete-common2 ()
+;;   (interactive)
+;;   (when (company-manual-begin)
+;;     (if (and (not (cdr company-candidates))
+;;              (equal company-common (car company-candidates)))
+;;         (company-complete-selection)
+;;       (company--insert-candidate2 company-common))))
 
-(define-key company-active-map [tab] 'company-complete-common2)
+;; (define-key company-active-map [tab] 'company-complete-common2)
+
+(define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
+
+(add-hook 'after-init-hook 'company-statistics-mode)
 
 ;; ;; Add yasnippet support for all company backends
 ;; ;; https://github.com/syl20bnr/spacemacs/pull/179
