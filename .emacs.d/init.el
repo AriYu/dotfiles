cd8cfad8d6269b11b3419de35b2b8d11d5ce3786
Arita Yuta <k104009y@mail.kyutech.jp>
2016-05-17 12:48:50 +0900

    add company setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -284,6 +284,7 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 (setq company-dabbrev-downcase nil)
+(setq company-dabbrev-ignore-case nil)
 ;; (add-hook 'c++-mode-hook 'company-mode)
 ;; (add-hook 'c-mode-hook 'company-mode)
 ;; (add-hook 'python-mode-hook 'company-mode)
